e4730166e40c3e14735515c8feb4e2e99162a32a
kunal kirola <kunalkirola@gmail.com>
2024-08-21 07:52:38 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Form1Component } from './pages/form1/form1.component';
+import { Form2Component } from './pages/form2/form2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '/', redirectTo: '/form1' },
+  { path: '/form1', component: Form1Component },
+  { path: '/form2', component: Form2Component },
+];
