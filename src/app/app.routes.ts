e5c6a6f37e0653827ad58725fa849f0c7c7640ac
kunal kirola <kunalkirola@gmail.com>
2024-08-22 08:16:52 +0530

    Explain FormControl properties and methods

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Form1Component } from './pages/form1/form1.component';
 import { Form2Component } from './pages/form2/form2.component';
 
 export const routes: Routes = [
-  { path: '/', redirectTo: '/form1' },
-  { path: '/form1', component: Form1Component },
-  { path: '/form2', component: Form2Component },
+  { path: '', redirectTo: 'form1', pathMatch: 'full' },
+  { path: 'form1', component: Form1Component },
+  { path: 'form2', component: Form2Component },
 ];
--- src/app/pages/form1/form1.component.ts
+++ src/app/pages/form1/form1.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form1',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form1.component.html',
-  styleUrl: './form1.component.css'
+  styleUrl: './form1.component.css',
 })
 export class Form1Component {
+  inputControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(30),
+  ]);
 
+  handleSubmit() {
+    console.log(this.inputControl.value);
+    console.log(this.inputControl.valid);
+    console.log(this.inputControl.invalid);
+    console.log(this.inputControl.status);
+    console.log(this.inputControl.touched);
+    console.log(this.inputControl.untouched);
+    console.log(this.inputControl.errors);
+    // this.inputControl.setValue('hello');
+    // this.inputControl.patchValue('hello david')
+    // this.inputControl.reset();
+    // this.inputControl.markAsTouched();
+    // this.inputControl.markAsUntouched();
+    // this.inputControl.disable();
+    // this.inputControl.enable();
+    // this.inputControl.clearValidators();
+    // this.inputControl.setValidators([Validators.required]); // only this validation get applied
+    // this.inputControl.updateValueAndValidity();
+    // this.inputControl.setErrors({ hello: true });
+    console.log(this.inputControl.hasError('required')) // find specific error
+    console.log(this.inputControl.errors);
+  }
+
+  // check if user name is admin then show error
+  checkUsername() {
+    // if (this.inputControl.value === 'admin') {
+    //   this.inputControl.setErrors({ reservedName: true });
+    // } else {
+    //   this.inputControl.setErrors(null);
+    // }
+  }
 }
